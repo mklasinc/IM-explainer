da4f6a202fdd4e8814c6cde447a24108ccd3941a
NYUAD <nyuad@C3-029-MBP-12.local>
2018-04-26 10:53:44 +0400

    massive restructuring ahead

--- public/interface/js/sketches/landing_page_sketch.js
+++ public/interface/js/sketches/landing_page_sketch.js
@@ -1,19 +1,24 @@
 var canvas_width = $('#canvas-container').width();
 var canvas_height = $('#canvas-container').height();
+console.log("canvas width is",canvas_width,"canvas_height is",canvas_height);
 var radius;
 var noise_start = 0;
-var ridge_base_y = canvas_height/2 + 100; //option 1: perspective lines
+// var ridge_base_y = canvas_height/2 + 100; //option 1: perspective lines
+var ridge_base_y = canvas_height * 0.75; //option 1: perspective lines
 // var ridge_base_y = canvas_height - 50; // option 2: mountains
 var horizon_y_offset = 100;
 var inc = 0.005;
 
+// perlin noise ridge variables
 var c1;
 var c2;
 var c3;
-// var c4 =
+var r1_c,r1_inc,r1_yoff,r2_c,r2_inc,r3_c,r4_c,r5_c;
+
 
 var yoff = 0.0;
 
+//gradient variables
 var c_white;
 var c_transparent;
 var Y_AXIS = 1;
@@ -22,6 +27,22 @@ var X_AXIS = 2;
 // setup
 
 function setup() {
+
+	// if(canvas_width > 1500){
+	// 	r1_inc = 0.03;
+	// 	r2_inc = 0.04;
+	// 	r3_yoff =
+	// 	r3_y =
+	// 	r4_yoff =
+	// 	r4_y =
+	// }else{
+	// 	r1_inc = 0.05;
+	// 	r2_inc = 0.05;
+	// 	r3_yoff =
+	// 	r3_y =
+	// 	r4_yoff =
+	// 	r4_y =
+	// };
 
 	// c1 = color(79,239,157);
 	// c2 = color(79,239,157,150);
@@ -32,73 +53,35 @@ function setup() {
 	c2 = color(255, 215, 119,180);
 	c3 = color(255, 0, 143,70);
 	c1 = color(255, 0, 77, 255);
-	console.log(canvas_width,canvas_height);
-	c_white = color(255,255,255,255);
-	c_transparent = color(0,0,0,0);
+
+	// c_white = color(255,255,255,255);
+	// c_transparent = color(0,0,0,0);
 	myCanvas = createCanvas(canvas_width , canvas_height);
   myCanvas.parent('canvas-container');
   radius = int(dist(canvas_width/2,canvas_height/2,0,0));
 
-  // draw_perspective_lines(25);
-
-	//
-  // draw_perlin_noise(150,200,200,cos);
-  // draw_perlin_noise(100,300,50,cos);
-  // draw_perlin_noise(200,300,240,cos);
-	// draw_perlin_noise(200,300,240,cos);
-    // rectMode(CENTER);
-  // fill(0);
-  // rect(0,canvas_height/2+20,canvas_width,2);
-
 	// setGradient(0, ridge_base_y + 100, width, height/2, c_white, c_transparent, Y_AXIS);
   // rectMode(CENTER);
 	fill(color('#2e3192'));
-  rect(0,ridge_base_y,canvas_width,200);
+  rect(0,ridge_base_y,canvas_width,canvas_height-ridge_base_y);
 	// setGradient(0, ridge_base_y + horizon_y_offset, width, height - ridge_base_y + horizon_y_offset, c_white, c_transparent, Y_AXIS);
+
 	//draw perspective lines
 	draw_perspective_lines(ridge_base_y + 10,20);
+	// draw_perlin_noise(ridge_y_off, ridge_height, ridge_color,ridge_func,)
+	draw_perlin_noise(canvas_height*0.2,canvas_height * 0.45,240,sin,noise_start,0.5); // furthest ridge
+	draw_perlin_noise(canvas_height*0.15,canvas_height * 0.5,240,cos,noise_start,0.5); // furthest ridge
+	draw_perlin_noise(canvas_height*0.35,canvas_height * 0.05,c3,sin,noise_start); // back ridge
+	draw_perlin_noise(canvas_height*0.2,canvas_height * 0.35,c2,cos,noise_start, 1,false,r2_inc); // middle ridges
+	draw_perlin_noise(canvas_height*0.01,canvas_height * 0.4,c1,cos,noise_start,0,true,r1_inc); // front ridge
+	fill(0);
+	// noLoop();
 
-	// draw perlin noise ridges
-	// draw_perlin_noise(100,250,240,sin,noise_start,0.5); // furthest ridge
-	// draw_perlin_noise(100,250,240,cos,noise_start,0.5); // furthest ridge
-	// draw_perlin_noise(250,50,220,sin,noise_start); // back ridge
-	// draw_perlin_noise(100,150,150,cos,noise_start, 1); // middle ridges
-	// draw_perlin_noise(20,300,50,cos,noise_start,0,true); // front ridge
-
-	draw_perlin_noise(100,250,240,sin,noise_start,0.5); // furthest ridge
-	draw_perlin_noise(100,250,240,cos,noise_start,0.5); // furthest ridge
-	draw_perlin_noise(250,50,c3,sin,noise_start); // back ridge
-	draw_perlin_noise(100,150,c2,cos,noise_start, 1); // middle ridges
-	draw_perlin_noise(20,300,c1,cos,noise_start,0,true); // front ridge
-	// camera.on();
-	noLoop();
 }
 
 // draw
 
 function draw() {
-
-	// console.log(mouseX,mouseY);
-
-	// // draw perlin noise ridges
-	// draw_perlin_noise(100,250,240,sin,noise_start,0.5); // furthest ridge
-	// draw_perlin_noise(100,250,240,cos,noise_start,0.5); // furthest ridge
-	// draw_perlin_noise(250,50,220,sin,noise_start); // back ridge
-	// draw_perlin_noise(100,150,150,cos,noise_start, 1); // middle ridges
-	// draw_perlin_noise(20,300,50,cos,noise_start); // front ridge
-
-	//.5 zoom is zooming out (50% of the normal size)
-	// if(mouseIsPressed)
-	// 	camera.zoom = .5;
-	// else
-	// 	camera.zoom = 1;
-	// draw_perlin_noise(100,250,240,sin,noise_start,0.5); // furthest ridge
-	// draw_perlin_noise(100,250,240,cos,noise_start,0.5); // furthest ridge
-	// draw_perlin_noise(250,50,c3,sin,noise_start); // back ridge
-	// draw_perlin_noise(100,150,c2,cos,noise_start, 1); // middle ridges
-	// draw_perlin_noise(20,300,c1,cos,noise_start,0,true); // front ridge
-
-
 	noise_start += inc;
 }
 
@@ -125,7 +108,7 @@ function draw_perspective_lines(base_y,num){
 
 // this function draws perlin noise
 
-function draw_perlin_noise(ridge_y_offset,ridge_height,col,sine_func,n_start = 0,start_offset = 0, ridge_flag = false){
+function draw_perlin_noise(ridge_y_offset,ridge_height,col,sine_func,n_start = 0,start_offset = 0, ridge_flag = false,sine_inc = 0.05){
   beginShape();
   fill(col);
   noStroke();
@@ -134,13 +117,13 @@ function draw_perlin_noise(ridge_y_offset,ridge_height,col,sine_func,n_start = 0
   // Iterate over horizontal pixels
   for (var x = 0; x <= canvas_width + 10; x += 10) {
 
-    var n = map(noise(xoff, n_start), 0, 1, -30,30);
+    var n = map(noise(xoff, n_start), 0, 1, -40,40);
 		var s = map(sine_func(xoff),-1,1,0,ridge_height);
 		var y = ridge_base_y  - ridge_y_offset - n - s;
     // Set the vertex
     vertex(x, y);
     // Increment x dimension for noise
-    xoff += 0.05;
+    xoff += sine_inc;
   };
 
 	vertex(canvas_width, ridge_base_y + 30);
